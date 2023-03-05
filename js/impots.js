2d7d9b5c3882b6e1503600bf5ff1d086fe23c8f7
Yann MONTENOT <montenot.yann@gmail.com>
2023-03-05 20:43:58 +0100

    100%

--- js/impots.js
+++ js/impots.js
@@ -4,13 +4,23 @@
  * and open the template in the editor.
  */
 
-function recupMontantImposition(revenu){
 
+function finale() {
+    let revenufinal = document.getElementById("i_revenu").value;
+
+
+    window.document.querySelector("#revenu").innerHTML = "Votre revenu étant de " + revenufinal + ", vos impots sont à hauteur de : " + recupMontantImposition(revenufinal) + "€";
+    
+}
+
+function recupMontantImposition(revenu) {
+
+    const tranche1 = 0.0;
     const tranche2 = 0.11;
     const tranche3 = 0.3;
     const tranche4 = 0.41;
     const tranche5 = 0.45;
-    
+
     const maxTr1 = 10777;
     const minTr2 = 10778;
     const maxTr2 = 27478;
@@ -19,57 +29,40 @@ function recupMontantImposition(revenu){
     const minTr4 = 78571;
     const maxTr4 = 168994;
     const minTr5 = 168995;
-    
-    
-   
-/*
- * Revenu tranche5
- */ 
-    if (revenu => minTr5){
-        result1 = (revenu - minTr5) * tranche5;
-        result2 = (maxTr4 - minTr4) * tranche4;
-        result3 = (maxTr3 - minTr3) * tranche3;
-        result4 = (maxTr2 - minTr2) * tranche2;
-        resulatfinal5 = result1 + result2 + result3 + result4;
-        
-/*
- * Revenu tranche4
- */ 
-        if(revenu < minTr5 && revenu >= minTr4){
-            result2 = (revenu - minTr4) * tranche4;
-            result3 = (maxTr3 - minTr3) * tranche3;
-            result4 = (maxTr2 - minTr2) * tranche2;
-            resulatfinal5 = result2 + result3 + result4;
-            
-/*
- * Revenu tranche3
- */ 
-            
-            if(revenu < minTr4 && revenu >= minTr3){
-                result3 = (revenu - minTr3) * tranche3;
-                result4 = (maxTr2 - minTr2) * tranche2;
-                resulatfinal5 = result3 + result4;
-                
-/*
- * Revenu tranche2
- */ 
-                
-                if(revenu < minTr3 && revenu >= minTr2){
-                    result1 = (revenu - minTr2) * tranche2;
-                    
-                    
-/*
- * Revenu tranche1
- */ 
-                    
-                    if(revenu <= maxTr1){
-                        resultfinal = revenu;
-                    }    
-                }
-               
-            }
-            
-        }
+
+
+    if (revenu >= minTr2 && revenu <= maxTr2) {
+        return (revenu - maxTr1) * tranche2;
     }
-   
+
+    else if (revenu >= minTr3 && revenu <= maxTr3) {
+        resul1 = (maxTr2 - maxTr1) * tranche2;
+        resul = (revenu - maxTr2) * tranche3;
+        return resultfinal = resul + resul1;
+    }
+
+    else if (revenu >= minTr4 && revenu <= maxTr4) {
+        res = (revenu - maxTr3) * tranche4;
+        res1 = (maxTr3 - maxTr2) * tranche3;
+        res2 = (maxTr2 - maxTr1) * tranche2;
+        return res + res1 + res2;
+    }
+
+    else if (revenu >= minTr5) {
+        result1 = (revenu - maxTr4) * tranche5;
+        result2 = (maxTr4 - maxTr3) * tranche4;
+        result3 = (maxTr3 - minTr2) * tranche3;
+        result4 = (maxTr2 - maxTr1) * tranche2;
+        return result1 + result2 + result3 + result4;
+    }
+    else {
+        return (revenu * tranche1);
+    }
 }
+
+window.addEventListener("load", function () {
+    window.document.querySelector("#i_revenu").addEventListener("input", finale);
+}
+);
+
+
